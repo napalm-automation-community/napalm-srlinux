bd245675076a8e53e069e20e2583c8a220fbcc4d
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2023-05-02 09:52:22 -0500

    Fix test logic

--- test/ci/load_commit_rollback.py
+++ test/ci/load_commit_rollback.py
@@ -24,6 +24,12 @@ set /interface ethernet-1/1 description "{DESC}"
 device.load_merge_candidate(config=cfg) # CLI format
 device.commit_config()
 
+# get config -> check that description is set
+config = device.get_config()
+parsed = json.loads(config["running"])
+print( json.dumps(parsed["srl_nokia-interfaces:interface"],indent=2) )
+assert( parsed["srl_nokia-interfaces:interface"][0]["description"] == DESC )
+
 cfg2 = {
  "interface": [
   { "name": "ethernet-1/1", "description": f"{DESC} using JSON" }
@@ -33,17 +39,18 @@ device.load_merge_candidate(config=json.dumps(cfg2)) # JSON format
 device.commit_config()
 
 # get config -> check that description is set
-config = device.get_config()
-parsed = json.loads(config["running"])
-print( json.dumps(parsed["srl_nokia-interfaces:interface"],indent=2) )
-assert( parsed["srl_nokia-interfaces:interface"][0]["description"] == DESC )
+config2 = device.get_config()
+parsed2 = json.loads(config2["running"])
+print( json.dumps(parsed2["srl_nokia-interfaces:interface"],indent=2) )
+assert( parsed2["srl_nokia-interfaces:interface"][0]["description"] == cfg2["interface"][0]["description"] )
 
+# Revert changes in most recent commit_config
 reply = device.rollback()
 print( reply )
 
-config2 = device.get_config()
-parsed2 = json.loads(config2["running"])
-assert( "description" not in parsed2["srl_nokia-interfaces:interface"][0] )
+config3 = device.get_config()
+parsed3 = json.loads(config3["running"])
+assert( parsed3["srl_nokia-interfaces:interface"][0]["description"] == DESC )
 
 device.close()
 
